4001608b140c94908fa5596fe1edaee9fadfadf5
pogi <aaroncallanga01@gmail.com>
2025-09-09 10:16:21 +0800

    MovieStreamController - movie-streaming-service

--- movie-streaming-service/src/main/java/com/callanga/movie_streaming_service/controller/MovieStreamController.java
+++ movie-streaming-service/src/main/java/com/callanga/movie_streaming_service/controller/MovieStreamController.java
@@ -1,19 +1,33 @@
 package com.callanga.movie_streaming_service.controller;
 
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 @RestController
 @RequestMapping("/streams")
 public class MovieStreamController {
 
-    @GetMapping("/videoPath}")
-    public ResponseEntity<InputStreamResource> streamVideo(@PathVariable String videoPath) {
+    private static final String VIDEO_DIRECTORY = "C:\\Users\\QC-SDO\\Desktop\\Microservice\\videos\\";
 
-
+    @GetMapping("/{videoPath}")
+    public ResponseEntity<InputStreamResource> streamVideo(@PathVariable String videoPath) throws FileNotFoundException {
+        File file = new File(VIDEO_DIRECTORY + videoPath);
+        if (file.exists()) {
+            InputStreamResource inputStreamResource = new InputStreamResource(new FileInputStream(file));
+            return ResponseEntity.ok()
+                                 .contentType(MediaType.parseMediaType("video/mp4"))
+                                 .body(inputStreamResource);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
